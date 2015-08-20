d4b2160643f5cda9f18b0e6e6ef49b732e9ead3b
Christian Meter <cmeter@googlemail.com>
2015-08-20 20:33:40 +0200

    Prepare script execution

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,10 +1,29 @@
 from flask import render_template
 from app import app
 
+import subprocess
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
     return render_template('index.html',
-                           title='Home',
                            user=user)
+
+
+
+
+## Include scripts
+
+# helloworld.py
+@app.route('/scripts/helloworld')
+def helloworld():
+    cmd = ["ls","-l"]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              stdin=subprocess.PIPE)
+    out, err = p.communicate()
+    print(type(out))
+    out = out.decode('utf-8')
+    err = err.decode('utf-8')
+    return render_template('index.html', out=out, err=err)
