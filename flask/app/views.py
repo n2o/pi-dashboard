3e9edbcaeb2906270890e9ead640f0a03963446b
Christian Meter <cmeter@googlemail.com>
2015-09-11 10:12:46 +0200

    Fix infinite loop, add assertion

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, Response
 from jinja2 import Environment, FileSystemLoader
 from app import app
+import re
 
 from pygments import highlight
 from pygments.lexers import BashLexer
@@ -18,20 +19,17 @@ def index():
 @app.route('/stream/<script>')
 def execute(script):
     def inner():
+        assert re.match(r'^[a-zA-Z._-]+$', script)
         exec_path = "scripts/" + script + ".py"
-        cmd = ["/usr/bin/env", "python3", "-u", exec_path]  # -u: don't buffer output
+        cmd = ["python3", "-u", exec_path]  # -u: don't buffer output
 
         proc = subprocess.Popen(
             cmd,
             stdout=subprocess.PIPE,
         )
 
-        for line in iter(proc.stdout.readline, ''):
+        for line in proc.stdout:
             yield highlight(line, BashLexer(), HtmlFormatter())
-            # If process is done, break loop
-   #         if proc.poll() == 0:
-   #             yield "<span id='stream_finished'></span>"
-   #             break
 
     env = Environment(loader=FileSystemLoader('app/templates'))
     tmpl = env.get_template('stream.html')
